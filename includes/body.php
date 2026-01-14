ff71c5559b19347d700b507a7379ee005e36bac2
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-14 10:30:30 +0530

    Update body.php

--- includes/body.php
+++ includes/body.php
@@ -87,3 +87,31 @@
 				</div>
 			</div>
 		</div>
+
+		<div class="section">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-12">
+						<div class="section-title">
+							<h3 class="title">New Products</h3>
+							<div class="section-nav">
+								<ul class="section-tab-nav tab-nav">
+									<li class="active"><a data-toggle="tab" href="#tab1">Laptops</a></li>
+									<li><a data-toggle="tab" href="#tab1">Smartphones</a></li>
+									<li><a data-toggle="tab" href="#tab1">Cameras</a></li>
+									<li><a data-toggle="tab" href="#tab1">Accessories</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-12 mainn mainn-raised">
+						<div class="row">
+							<div class="products-tabs">
+								<!-- tab -->
+								<div id="tab1" class="tab-pane active">
+									<div class="products-slick" data-nav="#slick-nav-1" >
+									
+									<?php
+                    include 'db.php';
