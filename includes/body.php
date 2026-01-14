1c87de1144b574345fe608dc7184896f224b68a5
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-14 10:29:47 +0530

    Update body.php

--- includes/body.php
+++ includes/body.php
@@ -30,3 +30,60 @@
       </div>
   
     </div>
+
+    <a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only" >Previous</span>
+    </a>
+    <a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+     
+
+
+		<div class="section mainn mainn-raised">
+		
+			<div class="container">
+			
+				<div class="row">
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=78"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/shop01.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Laptop<br>Collection</h3>
+								<a href="product.php?p=78" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=72"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/shop03.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Accessories<br>Collection</h3>
+								<a href="product.php?p=72" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=79"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/shop02.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Cameras<br>Collection</h3>
+								<a href="product.php?p=79" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+                            </div></a>
+					</div>
+				</div>
+			</div>
+		</div>
