408e88f961aa4699b3a5a58a8499a9fbdb2e1c94
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-14 10:32:52 +0530

    Update body.php

--- includes/body.php
+++ includes/body.php
@@ -170,3 +170,83 @@
       
 }
 ?>
+</div>
+									<div id="slick-nav-1" class="products-slick-nav"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+
+		<div id="hot-deal" class="section mainn mainn-raised">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="hot-deal">
+							<ul class="hot-deal-countdown">
+								<li>
+									<div>
+										<h3>02</h3>
+										<span>Days</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>10</h3>
+										<span>Hours</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>34</h3>
+										<span>Mins</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>60</h3>
+										<span>Secs</span>
+									</div>
+								</li>
+							</ul>
+							<h2 class="text-uppercase">hot deal this week</h2>
+							<p>New Collection Up to 50% OFF</p>
+							<a class="primary-btn cta-btn" href="store.php">Shop now</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		
+
+		<div class="section">
+			<div class="container">
+				<div class="row">
+
+
+					<div class="col-md-12">
+						<div class="section-title">
+							<h3 class="title">Top selling</h3>
+							<div class="section-nav">
+								<ul class="section-tab-nav tab-nav">
+									<li class="active"><a data-toggle="tab" href="#tab2">Formals</a></li>
+									<li><a data-toggle="tab" href="#tab2">Shirts</a></li>
+									<li><a data-toggle="tab" href="#tab2">T-Shirts</a></li>
+									<li><a data-toggle="tab" href="#tab2">Pants</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-12 mainn mainn-raised">
+						<div class="row">
+							<div class="products-tabs">
+								<div id="tab2" class="tab-pane fade in active">
+									<div class="products-slick" data-nav="#slick-nav-2">
+										<?php
+                    include 'db.php';
