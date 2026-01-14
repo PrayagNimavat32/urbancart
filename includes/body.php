8d48500a83d918ce9b801ae9a770fe867681e8d8
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-14 10:28:24 +0530

    Update body.php

---
+++ includes/body.php
@@ -0,0 +1,32 @@
+<div class="main main-raised">
+        <div class="container mainn-raised" style="width:100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+   
+
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="img/banner3.jpg" alt="Los Angeles" style="width:100%;">
+        
+      </div>
+
+      <div class="item">
+        <img src="img/banner2.jpg" style="width:100%;">
+        
+      </div>
+    
+      <div class="item">
+        <img src="img/banner4.jpg" alt="New York" style="width:100%;">
+        
+      </div>
+      <div class="item">
+        <img src="img/banner1.jpg" alt="New York" style="width:100%;">
+        
+      </div>
+      <div class="item">
+        <img src="img/banner3.jpg" alt="New York" style="width:100%;">
+        
+      </div>
+  
+    </div>
