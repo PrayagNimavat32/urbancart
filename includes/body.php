2bcd6ac28c0945f3964bfa70bf7942764c0109da
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-15 16:11:55 +0530

    Update body.php

--- includes/body.php
+++ includes/body.php
@@ -451,3 +451,15 @@
 							</div>
 						</div>
 					</div>
+
+					<div class="clearfix visible-sm visible-xs">
+					    
+					</div>
+
+					<div class="col-md-4 col-xs-6">
+						<div class="section-title">
+							<h4 class="title">Top selling</h4>
+							<div class="section-nav">
+								<div id="slick-nav-5" class="products-slick-nav"></div>
+							</div>
+						</div>
