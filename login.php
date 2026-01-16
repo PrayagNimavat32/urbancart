61c3a6f33b7fd8caea829442ae7ade8ed00c770b
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-16 14:20:20 +0530

    login.php
    user login page

--- login.php
+++ login.php
@@ -52,4 +52,35 @@ if(isset($_POST["email"]) && isset($_POST["password"])){
 				
 			
             exit;
+
+            }else{
+                $email = mysqli_real_escape_string($con,$_POST["email"]);
+                $password =md5($_POST["password"]) ;
+                $sql = "SELECT * FROM admin_info WHERE admin_email = '$email' AND admin_password = '$password'";
+                $run_query = mysqli_query($con,$sql);
+                $count = mysqli_num_rows($run_query);
+
+            if($count == 1){
+                $row = mysqli_fetch_array($run_query);
+                $_SESSION["uid"] = $row["admin_id"];
+                $_SESSION["name"] = $row["admin_name"];
+                $ip_add = getenv("REMOTE_ADDR");
+
+                    echo "login_success";
+
+                    echo "<script> location.href='admin/add_products.php'; </script>";
+                    exit;
+
+                }else{
+                    echo "<span style='color:red;'>Please register before login..!</span>";
+                    exit();
+                }
+    
+	
+}
+    
+	
+}
+
+?>
                 
