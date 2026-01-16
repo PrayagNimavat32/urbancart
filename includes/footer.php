650e63c5c0c2bfa26b394f39ee58314d6e97c6e8
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-16 13:57:27 +0530

    Update footer.php

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<footer id="footer">
+			<div class="section">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title">About Us</h3>
+								<p>This is my Small Database Management System mini project</p>
+								<ul class="footer-links">
+									<li><a href="#"><i class="fa fa-map-marker"></i>Rajkot , Gujrat</a></li>
+									<li><a href="#"><i class="fa fa-phone"></i>+91-8799186082</a></li>
+									<li><a href="#"><i class="fa fa-envelope-o"></i>prayagnimavat007@gmail.com</a></li>
+								</ul>
+							</div>
+						</div>
