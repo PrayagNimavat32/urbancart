74f3f9cb41118377400506655e1a5ddef8be40e9
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-16 14:14:59 +0530

    Update login.php

--- login.php
+++ login.php
@@ -6,3 +6,10 @@ session_start();
 if(isset($_POST["email"]) && isset($_POST["password"])){
 	$email = mysqli_real_escape_string($con,$_POST["email"]);
 	$password = $_POST["password"];
+    $sql = "SELECT * FROM user_info WHERE email = '$email' AND password = '$password'";
+	$run_query = mysqli_query($con,$sql);
+	$count = mysqli_num_rows($run_query);
+    $row = mysqli_fetch_array($run_query);
+		$_SESSION["uid"] = $row["user_id"];
+		$_SESSION["name"] = $row["first_name"];
+		$ip_add = getenv("REMOTE_ADDR");
