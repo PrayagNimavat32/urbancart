bf4daf98df02a749751d5b06aba0d9f5d42c1993
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-17 21:07:49 +0530

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -5,3 +5,13 @@ include "header.php";
 
                   
 ?>
+
+<style>
+
+.row-checkout {
+  display: -ms-flexbox; /* IE10 */
+  display: flex;
+  -ms-flex-wrap: wrap; /* IE10 */
+  flex-wrap: wrap;
+  margin: 0 -16px;
+}
