aac931ba790ddcd0926c39202ee0a39f7284482f
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-18 17:49:49 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -22,3 +22,17 @@ include "activitity.php";
           </div>
         </div>";
             }
+            ?></a>
+                </div>
+                <div class="col-md-14">
+            <div class="card ">
+              <div class="card-header card-header-primary">
+                <h4 class="card-title"> Users List</h4>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive ps">
+                  <table class="table table-hover tablesorter " id="">
+                    <thead class=" text-primary">
+                        <tr><th>ID</th><th>FirstName</th><th>LastName</th><th>Email</th><th>Password</th><th>Contact</th><th>Address</th><th>City</th>
+                    </tr></thead>
+                    <tbody>
