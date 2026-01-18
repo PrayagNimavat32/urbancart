0f35c542bbea374cb0fd7a4683e5f5e1241cef53
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-18 17:41:36 +0530

    Update login.php

---
+++ admin/login.php
@@ -0,0 +1,13 @@
+<?php include("./server/server.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Login</title>
+
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
