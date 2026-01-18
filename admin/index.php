013a507b71078cf755ae4c2bf308b5a786d16a90
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-18 17:50:15 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -36,3 +36,19 @@ include "activitity.php";
                         <tr><th>ID</th><th>FirstName</th><th>LastName</th><th>Email</th><th>Password</th><th>Contact</th><th>Address</th><th>City</th>
                     </tr></thead>
                     <tbody>
+                        <?php 
+                        $result=mysqli_query($con,"select * from user_info")or die ("query 1 incorrect.....");
+
+                        while(list($user_id,$first_name,$last_name,$email,$password,$phone,$address1,$address2)=mysqli_fetch_array($result))
+                        {	
+                        echo "<tr><td>$user_id</td><td>$first_name</td><td>$last_name</td><td>$email</td><td>$password</td><td>$phone</td><td>$address1</td><td>$address2</td>
+
+                        </tr>";
+                        }
+                        ?>
+                    </tbody>
+                  </table>
+                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
+              </div>
+            </div>
+          </div>
