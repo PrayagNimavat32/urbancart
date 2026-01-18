0f222f3095f6467b308a0fe9a3b758f76c5aea36
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-18 17:49:19 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -7,3 +7,18 @@ include "topheader.php";
 include "activitity.php";
 
 ?>
+
+<div class="content">
+        <div class="container-fluid">
+         <div class="panel-body">
+		<a>
+            <?php  //success message
+            if(isset($_POST['success'])) {
+            $success = $_POST["success"];
+            echo "<div class='col-md-12 col-xs-12' id='product_msg'>
+          <div class='alert alert-success'>
+            <a href='#'' class='close' data-dismiss='alert' aria-label='close'>×</a>
+            <b>Product is Added..!</b>
+          </div>
+        </div>";
+            }
