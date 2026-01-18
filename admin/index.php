99a44dca23b7e29b1ade5bfabbd055b0497e7ddc
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-18 17:52:42 +0530

    index.php
    admin home page

--- admin/index.php
+++ admin/index.php
@@ -121,3 +121,39 @@ include "activitity.php";
             </div>
           </div>
            </div>
+           <div class="col-md-5">
+            <div class="card ">
+              <div class="card-header card-header-primary">
+                <h4 class="card-title">Subscribers</h4>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive ps">
+                  <table class="table table-hover tablesorter " id="">
+                    <thead class=" text-primary">
+                        <tr><th>ID</th><th>email</th>
+                    </tr></thead>
+                    <tbody>
+                      <?php 
+                        $result=mysqli_query($con,"select * from email_info")or die ("query 1 incorrect.....");
+
+                        while(list($brand_id,$brand_title)=mysqli_fetch_array($result))
+                        {	
+                        echo "<tr><td>$brand_id</td><td>$brand_title</td>
+
+                        </tr>";
+                        }
+                        ?>
+                    </tbody>
+                  </table>
+                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
+              </div>
+            </div>
+          </div>
+           
+            
+          
+        </div>
+      </div>
+      <?php
+include "footer.php";
+?>
