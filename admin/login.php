c4859e118bab4e11700705a484f84dc0f99f348b
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-18 17:42:37 +0530

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -24,3 +24,28 @@
                         
                         
                     </div>
+
+                    <div class="signin-form">
+                        <h2 class="form-title">ADMIN LOGIN</h2>
+                        <form  class="register-form" id="login-form" action="login.php" method="post">
+                            <div class="alert alert-danger"><h4 id="e_msg"><?php include('./server/errors.php'); ?></h4></div>
+                            <div class="form-group">
+                                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="admin_username" id="your_name" placeholder="Admin Email"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="your_pass" placeholder="Password"/>
+                            </div>
+                           
+                            <div class="form-group form-button">
+                                <input type="submit" name="login_admin" id="signin" class="form-submit" value="Log in"/>
+                            </div>
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
