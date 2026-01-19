24536dbfcc16bb55f0716041e52224400212512c
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-19 17:29:48 +0530

    Update store.php

--- store.php
+++ store.php
@@ -77,3 +77,16 @@ include 'header.php';
 										<option value="1">Position</option>
 									</select>
 								</label>
+
+                                label>
+									Show:
+									<select class="input-select">
+										<option value="0">20</option>
+										<option value="1">50</option>
+									</select>
+								</label>
+							</div>
+							<ul class="store-grid">
+								<li class="active"><i class="fa fa-th"></i></li>
+								<li><a href="#"><i class="fa fa-th-list"></i></a></li>
+							</ul>
