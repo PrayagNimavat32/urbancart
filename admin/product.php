43621a844c6354da550aaaef81bc8207c6f71c55
PrayagNimavat32 <161615579+PrayagNimavat32@users.noreply.github.com>
2026-01-20 22:04:55 +0530

    Update product.php

---
+++ admin/product.php
@@ -0,0 +1,11 @@
+<?php
+include "header.php";
+?>
+		<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},900);
+				});
+			});
+</script>
